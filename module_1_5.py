b7fe65f4bdc44c4f7f37d7a51a47c890032b099d
8matv1k8 <107505791+8matv1k8@users.noreply.github.com>
2024-11-20 21:42:11 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = [1, 3, 4, 5], 'mom, omnomnom', 10, 2, 3
+print('Immutable tuple:', immutable_var)
+# кортеж не позволяет изменять данные ввиду определения структуры данных
+mutable_list = [1, 2, 3, 4, 5, 6]
+mutable_list[0] = 'no'
+print('Mutable list:', mutable_list)
