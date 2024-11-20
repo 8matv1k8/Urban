cebf28816224e275151682a142a07ecb37a450d2
8matv1k8 <107505791+8matv1k8@users.noreply.github.com>
2024-11-20 17:53:15 +0300

    Add files via upload

---
+++ varias.py
@@ -0,0 +1,6 @@
+works_count = 12
+hours_count = 1.5
+objectOfLearning = "Python"
+timeLimit = hours_count / works_count
+print(f"Курс: {objectOfLearning}, всего задач: {works_count}, \
+затрачено часов: {hours_count}, среднее время выполнения {timeLimit} часа.")
