fe2e7d9c0e4c59413ee54d3220d63b3673a0b2a0
8matv1k8 <107505791+8matv1k8@users.noreply.github.com>
2024-11-28 00:33:11 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,7 +1,7 @@
 first, second, third = map(int, input('Введите три целых числа: ').split())
 if first == second == third:
     print('3')
-elif first == second or first == third or second == third:
+elif len(set([first, second, third])) == 2:
     print('2')
 else:
     print('0')
